c86f990761bb94b986c65b4c4031ba6e59af641a
ricardonfarias <82242026+ricardonfarias@users.noreply.github.com>
2021-05-25 22:02:01 +0100

    Delete framework-level-master directory

--- framework-level-master/src/main/java/util/Funcoes.java
+++
@@ -1,9 +0,0 @@
-package util;
-//classe criada para conter funções comuns entre todas as outras classes
-public class Funcoes {
-
-    public static Double removerCifraoDevolverDouble(String texto){
-        texto = texto.replace("$", "");
-        return Double.parseDouble(texto);
-    }
-}
